fd1ee0d6079b5dc3c3e1b6b7c1190147aa6666b1
Samuel Gardiner <samuel.gardiner@students.mq.edu.au>
2020-04-14 10:51:02 +0000

    Added Q1dii SQL answer and executable script.

---
+++ q1dii.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mysql -p -u awkologist compbiol -e \
+	"SELECT DISTINCT a1.metabolism
+ 	FROM annotation a1
+		INNER JOIN annotation a2
+		ON (a1.metabolism = a2.metabolism
+			AND a1.gene <> a2.gene)"  
---
+++ q1dii.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT a1.metabolism
+FROM annotation a1
+	INNER JOIN annotation a2
+	ON (a1.metabolism = a2.metabolism
+		AND a1.gene <> a2.gene)
