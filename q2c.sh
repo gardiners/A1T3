204f1e1a29fa95bdc45eeafc0309b8d16a9953fe
Samuel Gardiner <samuel.gardiner@students.mq.edu.au>
2020-04-15 08:22:15 +0000

    Corrected Q2c. Resolves #2.

--- q2c.sh
+++ q2c.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-mysql -p -u awkologist compbiol <<%%
+echo "SELECT DISTINCT m.MemberID, m.LastName, m.FirstName
+           FROM Member m
+	        INNER JOIN Entry e
+		        ON m.MemberID = e.MemberID
+	  WHERE NOT EXISTS (SELECT * FROM Entry e
+			     WHERE m.MemberID = e.MemberID
+			       AND e.Year = 2014);"
+
+mysql -p -u awkologist compbiol -e "
 SELECT DISTINCT m.MemberID, m.LastName, m.FirstName
            FROM Member m
-	        INNER JOIN  Entry e
-		         ON  m.MemberID = e.MemberID
-	  WHERE e.Year <> 2014;
-%%
-
+	        INNER JOIN Entry e
+		        ON m.MemberID = e.MemberID
+	  WHERE NOT EXISTS (SELECT * FROM Entry e
+			     WHERE m.MemberID = e.MemberID
+			       AND e.Year = 2014);"
--- q2c.sql
+++ q2c.sql
@@ -2,5 +2,6 @@ SELECT DISTINCT m.MemberID, m.LastName, m.FirstName
            FROM Member m
 	        INNER JOIN Entry e
 		        ON m.MemberID = e.MemberID
-	  WHERE e.Year <> 2014;
-
+	  WHERE NOT EXISTS (SELECT * FROM Entry e
+			     WHERE m.MemberID = e.MemberID
+			       AND e.Year = 2014);
